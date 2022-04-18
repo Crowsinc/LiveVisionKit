14bb881ccbcf4f9723ec92a71be4db9a444316be
Crowsinc <crowsinc.dev@gmail.com>
2022-04-18 18:39:08 +1000

    Module: Integrated interop functionality

--- LiveVisionKit/OBS/Plugin/Module.cpp
+++ LiveVisionKit/OBS/Plugin/Module.cpp
@@ -19,6 +19,7 @@
 #include <opencv2/core/ocl.hpp>
 
 #include "Diagnostics/Directives.hpp"
+#include "OBS/Interop/FrameIngest.hpp"
 
 //---------------------------------------------------------------------------------------------------------------------
 
@@ -41,9 +42,22 @@ void register_vs_source();
 
 void register_adb_source();
 
+void register_adb_effect_source();
+
 void register_lc_source();
 
 void register_cct_source();
+
+//---------------------------------------------------------------------------------------------------------------------
+
+void attach_ocl_interop_context(void* param, uint32_t cx, uint32_t cy)
+{
+	// NOTE: Attach (create) OpenCV's OpenCL context based on the graphics
+	// context in use by OBS. Do it here before any OpenCL code is able to
+	// run, then immediately remove the callback.
+	lvk::ocl::try_attach_graphics_interop_context();
+	obs_remove_main_render_callback(&attach_ocl_interop_context, nullptr);
+}
 
 //---------------------------------------------------------------------------------------------------------------------
 
@@ -52,19 +66,23 @@ bool obs_module_load()
 	register_fsr_source();
 	register_cas_source();
 
-	// Vision filters must use OpenCL
+	// Vision Async Filters 
 	if(cv::ocl::haveOpenCL())
 	{
-		cv::ocl::setUseOpenCL(true);
-
 		register_lc_source();
 		register_vs_source();
 		register_adb_source();
 
-		// Tool filters
 		register_cct_source();
 	}
 	else LVK_ERROR("OpenCL unsupported");
+
+	// Vision Effects Filters
+	if (lvk::ocl::supports_graphics_interop())
+	{
+		obs_add_main_render_callback(&attach_ocl_interop_context, nullptr);
+	}
+	else LVK_ERROR("OpenCL Interop unsupported");
 
 	return true;
 }
