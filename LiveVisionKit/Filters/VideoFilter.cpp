5d221fccc3b23a088dbc4c3ce0677cdce2191ec6
Crowsinc <crowsinc.dev@gmail.com>
2022-11-20 07:56:38 +1100

    Added basic video filter API

---
+++ LiveVisionKit/Filters/VideoFilter.cpp
@@ -0,0 +1,126 @@
+//    *************************** LiveVisionKit ****************************
+//    Copyright (C) 2022  Sebastian Di Marco (crowsinc.dev@gmail.com)
+//
+//    This program is free software: you can redistribute it and/or modify
+//    it under the terms of the GNU General Public License as published by
+//    the Free Software Foundation, either version 3 of the License, or
+//    (at your option) any later version.
+//
+//    This program is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//    GNU General Public License for more details.
+//
+//    You should have received a copy of the GNU General Public License
+//    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+// 	  **********************************************************************
+
+#include "VideoFilter.hpp"
+
+namespace lvk
+{
+//---------------------------------------------------------------------------------------------------------------------
+
+    VideoFilter::VideoFilter(const std::string filter_name, const uint32_t timing_samples)
+        : m_Alias(filter_name + std::to_string(this->uid())),
+          m_FrameTimer(timing_samples)
+    {}
+
+//---------------------------------------------------------------------------------------------------------------------
+
+    void VideoFilter::process(cv::UMat& frame, const bool debug)
+    {
+        m_FrameTimer.start();
+        filter(frame, debug);
+        m_FrameTimer.stop();
+    }
+
+//---------------------------------------------------------------------------------------------------------------------
+
+    void VideoFilter::process(
+        cv::VideoCapture& input_stream, 
+        cv::VideoWriter& output_stream,
+        const bool debug,
+        const std::function<void(VideoFilter&, cv::UMat&)> callback
+    )
+    {
+        LVK_ASSERT(input_stream.isOpened());
+        LVK_ASSERT(output_stream.isOpened());
+        
+        cv::UMat frame(cv::UMatUsageFlags::USAGE_ALLOCATE_DEVICE_MEMORY);
+        while(input_stream.read(frame))
+        {
+            process(frame, debug);
+            
+            if(!frame.empty())
+                output_stream.write(frame);
+            
+            callback(*this, frame);
+        }
+    }
+
+//---------------------------------------------------------------------------------------------------------------------
+
+    void VideoFilter::process(cv::UMat& frame, Logger& logger, const bool debug)
+    {
+        process(frame, debug);
+        write_log(logger);
+    }
+   
+//---------------------------------------------------------------------------------------------------------------------
+
+    void VideoFilter::process(
+        cv::VideoCapture& input_stream,
+        cv::VideoWriter& output_stream,
+        Logger& logger,
+        const bool debug,
+        const std::function<void(VideoFilter&, cv::UMat&, Logger&)> callback
+    )
+    {
+        process(
+            input_stream,
+            output_stream,
+            debug,
+            [&](VideoFilter& filter, cv::UMat& frame)
+            {
+                write_log(logger);
+                callback(filter, frame, logger);
+            }
+        );
+    }
+
+//---------------------------------------------------------------------------------------------------------------------
+
+    const Time VideoFilter::runtime() const
+    {
+        return m_FrameTimer.elapsed();
+    }
+
+//---------------------------------------------------------------------------------------------------------------------
+
+    const Time VideoFilter::runtime_average() const
+    {
+        return m_FrameTimer.average();
+    }
+
+//---------------------------------------------------------------------------------------------------------------------
+
+    float VideoFilter::runtime_consistency() const
+    {
+        return m_FrameTimer.consistency();
+    }
+
+//---------------------------------------------------------------------------------------------------------------------
+
+    const std::string& VideoFilter::alias() const
+    {
+        return m_Alias;
+    }
+
+//---------------------------------------------------------------------------------------------------------------------
+
+    void VideoFilter::write_log(Logger& logger) {}
+
+//---------------------------------------------------------------------------------------------------------------------
+
+}
---
+++ LiveVisionKit/Filters/VideoFilter.hpp
@@ -0,0 +1,85 @@
+//    *************************** LiveVisionKit ****************************
+//    Copyright (C) 2022  Sebastian Di Marco (crowsinc.dev@gmail.com)
+//
+//    This program is free software: you can redistribute it and/or modify
+//    it under the terms of the GNU General Public License as published by
+//    the Free Software Foundation, either version 3 of the License, or
+//    (at your option) any later version.
+//
+//    This program is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//    GNU General Public License for more details.
+//
+//    You should have received a copy of the GNU General Public License
+//    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+// 	  **********************************************************************
+
+#pragma once
+
+
+#include <functional>
+#include <opencv2/opencv.hpp>
+#include <opencv2/videoio.hpp>
+
+#include "Utility/Data/Unique.hpp"
+#include "Utility/Timing/TickTimer.hpp"
+#include "Diagnostics/Logging/Logger.hpp"
+
+namespace lvk
+{
+
+	class VideoFilter : public Unique<VideoFilter>
+	{
+	public:
+
+		VideoFilter(const std::string filter_name, const uint32_t timing_samples = 300);
+
+		virtual ~VideoFilter() = default;
+
+		void process(
+			cv::UMat& frame,
+			const bool debug = false
+		);
+		
+		void process(
+			cv::VideoCapture& input_stream,
+			cv::VideoWriter& output_stream,
+			const bool debug = false,
+			const std::function<void(VideoFilter&,cv::UMat&)> callback = [](auto&,auto&) {}
+		);
+
+		void process(
+			cv::UMat& frame,
+			Logger& logger,
+			const bool debug = false
+		);
+
+		void process(
+			cv::VideoCapture& input_stream,
+			cv::VideoWriter& output_stream,
+			Logger& logger,
+			const bool debug = false,
+			const std::function<void(VideoFilter&, cv::UMat&, Logger&)> callback = [](auto&, auto&, auto&) {}
+		);
+	
+		const Time runtime() const;
+
+		const Time runtime_average() const;
+
+		float runtime_consistency() const;
+
+		const std::string& alias() const;
+
+	protected:
+
+		virtual void filter(cv::UMat& frame, const bool debug) = 0;
+
+		virtual void write_log(Logger& logger);
+
+	private:
+		Stopwatch m_FrameTimer;
+		std::string m_Alias;
+	};
+
+}
