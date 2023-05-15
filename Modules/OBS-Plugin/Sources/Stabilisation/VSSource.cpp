a7db415c987d86057a1c8d7dd9e83f6ddc7655be
Crowsinc <crowsinc.dev@gmail.com>
2023-05-16 08:11:31 +1000

    VSSource: Removed wrong render hook

--- Modules/OBS-Plugin/Sources/Stabilisation/VSSource.cpp
+++ Modules/OBS-Plugin/Sources/Stabilisation/VSSource.cpp
@@ -35,7 +35,6 @@ extern void register_vs_source()
 	config.destroy = lvk::dispatch::filter_delete<lvk::VSFilter>;
 
 	config.update = lvk::dispatch::filter_configure<lvk::VSFilter>;
-	config.video_render = lvk::dispatch::filter_render<lvk::VSFilter>;
 	config.filter_video = lvk::dispatch::filter_process<lvk::VSFilter>;
 
 	config.get_name = [](void* data) {return  L("vs.name"); };
